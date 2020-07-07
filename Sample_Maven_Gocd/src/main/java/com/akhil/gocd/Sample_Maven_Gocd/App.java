3b59db6af503935d3ec286c7f29d9c603aabde49
LENOVO <ksattaru@mdsol.com>
2020-07-07 11:28:51 +0530

    my first commit of sample maven

---
+++ Sample_Maven_Gocd/src/main/java/com/akhil/gocd/Sample_Maven_Gocd/App.java
@@ -0,0 +1,13 @@
+package com.akhil.gocd.Sample_Maven_Gocd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Sample_Maven_Gocd/src/test/java/com/akhil/gocd/Sample_Maven_Gocd/AppTest.java
@@ -0,0 +1,20 @@
+package com.akhil.gocd.Sample_Maven_Gocd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
